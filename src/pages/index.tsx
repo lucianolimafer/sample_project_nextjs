0df5dc2556e6d1e27169cf7191a06835cbfd91bd
lucianolimafer <lucianolimaf3r@gmail.com>
2021-02-13 17:20:13 -0300

    :rocket: Fecth data on Client Side Fetching concept

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react';
 import * as S from '../styles/pages/Home';
 
+interface IProduct {
+  id: number;
+  title: string;
+  price: number;
+  category_id: string;
+  slug: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      });
+    });
+  }, []);
+
   return (
     <div>
-      <S.Title>Teste</S.Title>
+      <S.Title>Products Recommended</S.Title>
+
+      <ul>
+        {recommendedProducts.map(recommendedProduct => {
+          return (
+            <li key={recommendedProduct.id}>
+              <S.Title>{recommendedProduct.title}</S.Title>
+              <S.Title>{recommendedProduct.price}</S.Title>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
